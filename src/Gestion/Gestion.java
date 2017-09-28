79e392810ca08ad41c70226d7a2e01963c323c7a
Lissette Alejandra Guadalupe Ahumada Castellanos <liss.ac@hotmail.com>
2017-09-28 14:08:10 -0500

    Primer Commit

---
+++ src/Gestion/Gestion.java
@@ -0,0 +1,13 @@
+
+package Gestion;
+
+/**
+ * @author Lissette Ahumada Castellanos
+ */
+public class Gestion {
+	public static void main(String[] args) {
+		System.out.println ("Gestion 1.0");
+
+	}
+
+}
